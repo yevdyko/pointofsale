9f72b2154557012588dfcf2bbee271d5290eea26
Yev Dyko <evgeny.dyko@gmail.com>
2016-09-23 21:50:47 +0300

    Add JS to open and close user modal window

--- src/js/script.js
+++ src/js/script.js
@@ -16,11 +16,9 @@ if (hamburger != null) {
 
 $('.js-tab-control').on('click', function(event){
   event.preventDefault();
-  var parent = $(this).parents('.js-tab-container');
+  var parent = $(this).parents('.catalog-control');
   parent.find('.js-tab-control').removeClass('active');
   $(this).addClass('active');
-  parent.find('.js-tab').removeClass('js-tab--open');
-  parent.find('.' + $(this).attr('data-tab') ).addClass('js-tab--open');
 })
 
 // Modal Windows
@@ -29,6 +27,10 @@ var button_checkout = document.querySelector('.order-control .btn--checkout');
 var button_checkout_close = document.querySelector('.js-modal-close--checkout');
 var modal_content_checkout = document.querySelector('.modal-content-wrapper--checkout');
 
+var button_username = document.querySelector('.user .user__link');
+var button_username_close = document.querySelector('.js-modal-close--username');
+var modal_content_username = document.querySelector('.modal-content-wrapper--username');
+
 if (button_checkout != null) {
   button_checkout.addEventListener('click', function(event) {
     event.preventDefault();
@@ -36,10 +38,24 @@ if (button_checkout != null) {
   });
 }
 
+if (button_username != null) {
+  button_username.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal_content_username.classList.add('modal-content-wrapper__show');
+  });
+}
+
 if (button_checkout_close != null) {
   button_checkout_close.addEventListener('click', function(event) {
     event.preventDefault();
     modal_content_checkout.classList.remove('modal-content-wrapper__show');
+  });
+}
+
+if (button_username_close != null) {
+  button_username_close.addEventListener('click', function(event) {
+    event.preventDefault();
+    modal_content_username.classList.remove('modal-content-wrapper__show');
   });
 }
 
